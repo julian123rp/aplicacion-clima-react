abdcee38317a52e0a7e5a62139aac502b4c86b9f
julian rodriguez <julianrp0805@gmail.com>
2024-07-25 12:20:40 -0500

    Realizamos una aplicacion de clima usando una api

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Proyect } from './Proyect'
+import "./style.css";
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Proyect/>
+  </React.StrictMode>,
+)
